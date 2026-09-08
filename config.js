2ebfbd6ccee99d3bfb63d4e5b1a4cb5b158ac7a0
Lollerr <cold_fusion132@yahoo.com>
2026-09-08 15:12:25 -0500

    Add files via upload

--- config.js
+++ config.js
@@ -2,5 +2,5 @@
 // Paste the public HTTPS URL of your Cloudflare Worker below.
 // Example: https://ap-bankroll-api.your-subdomain.workers.dev
 window.AP_CONFIG = {
-  API_URL: 'ap-bankroll-api.cold-fusion132.workers.dev'
+  API_URL: 'https://ap-bankroll-api.cold-fusion132.workers.dev'
 };
